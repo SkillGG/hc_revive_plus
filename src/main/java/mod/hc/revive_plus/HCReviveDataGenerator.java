3427c52af48953eff9b23bb66122ccffd5be6cb3
SkillGG <skilletssgames@gmail.com>
2024-07-04 11:49:54 +0200

    Add template

---
+++ src/main/java/mod/hc/revive_plus/HCReviveDataGenerator.java
@@ -0,0 +1,11 @@
+package mod.hc.revive_plus;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class HCReviveDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
